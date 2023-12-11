d69dc32d39b336cefcf56cdec388572f54f47840
Gabriel Paiva <1470350@sga.pucminas.br>
2023-12-11 12:46:06 -0300

    Estilizado tela de finalização

--- WindowsFormsApp1/Form5.Designer.cs
+++ WindowsFormsApp1/Form5.Designer.cs
@@ -28,19 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form5));
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Produtos = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.TOTAL = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonFNZ = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(2, -2);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(890, 218);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Lime;
+            this.label1.Location = new System.Drawing.Point(242, 229);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(410, 36);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "CONCLUSÃO DE COMPRA";
+            // 
+            // Produtos
+            // 
+            this.Produtos.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Produtos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1});
+            this.Produtos.HideSelection = false;
+            this.Produtos.Location = new System.Drawing.Point(233, 278);
+            this.Produtos.Name = "Produtos";
+            this.Produtos.Size = new System.Drawing.Size(438, 196);
+            this.Produtos.TabIndex = 6;
+            this.Produtos.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Hamburguers";
+            // 
+            // TOTAL
+            // 
+            this.TOTAL.AutoSize = true;
+            this.TOTAL.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TOTAL.Location = new System.Drawing.Point(25, 532);
+            this.TOTAL.Name = "TOTAL";
+            this.TOTAL.Size = new System.Drawing.Size(119, 32);
+            this.TOTAL.TabIndex = 7;
+            this.TOTAL.Text = "TOTAL:\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(173, 532);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 32);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "$$\r\n";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // buttonFNZ
+            // 
+            this.buttonFNZ.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.buttonFNZ.Location = new System.Drawing.Point(647, 505);
+            this.buttonFNZ.Name = "buttonFNZ";
+            this.buttonFNZ.Size = new System.Drawing.Size(203, 59);
+            this.buttonFNZ.TabIndex = 9;
+            this.buttonFNZ.Text = "FINALIZAR COMPRA\r\n";
+            this.buttonFNZ.UseVisualStyleBackColor = false;
             // 
             // Form5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(892, 587);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(889, 588);
+            this.Controls.Add(this.buttonFNZ);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TOTAL);
+            this.Controls.Add(this.Produtos);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.ForeColor = System.Drawing.Color.Lime;
             this.Name = "Form5";
             this.Text = "Form5";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView Produtos;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.Label TOTAL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonFNZ;
     }
 }
